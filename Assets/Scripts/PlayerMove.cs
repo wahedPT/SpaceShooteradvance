ea93e023c07bbb566e32b43967113ef19f22d49c
wahedxcube <wahedalikhan.md@xcubelabs.com>
2021-08-16 04:42:25 -0700

    playermovement done

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 {
+    [SerializeField]
+    private float PmoveSpeed;
+    [SerializeField]
+    private float horizontalInput;
+    [SerializeField]
+    private float verticalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,46 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+       
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * PmoveSpeed*horizontalInput);
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * PmoveSpeed * verticalInput);
+
+
+        // bounds for y
+        if(transform.position.y> 2f)
+        {
+            transform.position = new Vector3(transform.position.x, 2f, 0);
+
+        }
+        else if(transform.position.y<-4.2f)
+
+        {
+            transform.position = new Vector3(transform.position.x, -4.2f, 0);
+        }
+
+        // bounds for x
+        //if (transform.position.x > 8.2f)
+        //{
+        //    transform.position = new Vector3(8.2f, transform.position.y, 0);
+
+        //}
+        //else if (transform.position.x < -8.2f)
+
+        //{
+        //    transform.position = new Vector3( -8.2f,transform.position.y, 0);
+        //}
+        //bounds sharing
+        if (transform.position.x > 9.5f)
+        {
+            transform.position = new Vector3(-9.5f, transform.position.y, 0);
+
+        }
+        else if (transform.position.x < -9.5f)
+
+        {
+            transform.position = new Vector3(9.5f, transform.position.y, 0);
+        }
     }
 }
